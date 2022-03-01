00ec5e690b3f9781f159eb4ad7d00c8b1ee8aaf9
Camilla Cortney <camilla.cortney@gmail.com>
2022-02-28 20:37:16 -0800

    added some comments

--- api.py
+++ api.py
@@ -2,5 +2,6 @@ import requests
 
 api_url = "http://www.boredapi.com/api/activity?"
 
+# calls the api and returns a random activity
 def get_random_activity():
     return requests.get(api_url).json()
--- static/main.js
+++ static/main.js
@@ -1,6 +1,7 @@
+// find the DOM element where random activities will appear on the main homepage
 const $randomActivityHome = $("#random-home");
 
-
+// Returns the HTML markup for an activity
 function generateActivity(activity) {
     return `
         <div id = ${activity.key}>
@@ -13,6 +14,10 @@ function generateActivity(activity) {
         </div>`
 }
 
+// call the /random route which will return a random activity
+// call the generateActivity function on that activity
+// clear the current activity shown on the interface
+// Show the new activity
 async function get_random() {
     const activityData = await axios.get(`http://127.0.0.1:5000/random`);
     console.log(activityData);
@@ -21,6 +26,8 @@ async function get_random() {
     $randomActivityHome.append(activity);
 }
 
+// when on homepage without logged in user, get random activity. 
+// regenerate that random activity every 5 seconds
 if (window.location.href == 'http://127.0.0.1:5000/') {
     get_random();
     setInterval(get_random, 5000);
